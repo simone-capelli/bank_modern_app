e644993ee3c508536db6a055d30d767f6e75776e
simo-sw <simonecapelli13@gmail.com>
2023-07-06 14:51:15 +0200

    Card Deal section! :sparkles:

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({ styles }) => (
   <button
     type="button"
     className={`py-4 px-6 bg-blue-gradient 
-  font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}
+  font-poppins font-medium text-[18px] rounded-[10px] text-primary outline-none ${styles}`}
   >
     Get Started
   </button>
